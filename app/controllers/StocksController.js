49c1f593ced209dbfed5199c1b9b0bf50216ecc2
zachloubier <zach@growwithtrellis.com>
2015-09-07 13:45:48 -0700

    Style lists, add more info

--- app/controllers/StocksController.js
+++ app/controllers/StocksController.js
@@ -1,16 +1,9 @@
-app.controller('StocksController', ['$scope', 'stocks', function($scope) {
-	// stocks.success(function(data) {
-	// 	$scope.stocks = data;
-	// });
-
-	// $scope.stocks = [
-	// 	{
-	// 		name: 'Fitbit',
-	// 		price: 42.54
-	// 	},
-	// 	{
-	// 		name: 'Wayfair',
-	// 		price: 42.54
-	// 	}
-	// ];
+app.controller('StocksController', ['$scope', 'stocks', '$routeParams', function($scope, stocks, $routeParams) {
+	stocks.success(function(data) {
+		for (var i = 0; i < data.length; i++) {
+			if (data[i].Symbol == $routeParams.id) {
+				$scope.stock = data[i];
+			}
+		}
+	});
 }]);
