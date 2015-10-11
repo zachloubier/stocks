022c2215675e27e4efe91683e0ea5e520d76ce0e
zachloubier <zach@growwithtrellis.com>
2015-10-10 20:16:52 -0400

    Add resolve property to home route to auto get stocks from express

--- app/app.js
+++ app/app.js
@@ -6,11 +6,18 @@ var app = angular.module('Stocks', ['ngRoute', 'ngResource']);
 app.config(function($routeProvider) {
   $routeProvider.when('/', {
   	controller: 'HomeController',
-  	templateUrl: 'views/home/home.html'
+  	templateUrl: 'views/home/home.html',
+    resolve: {
+      postPromise: ['stocks', function(stocks) {
+        return stocks.getAll();
+      }]
+    }
   })
   .when('/stocks/:id', {
   	controller: 'StocksController',
   	templateUrl: 'views/stocks/stock.html'
   })
-  .otherwise({redirectTo: '/'});
+  .otherwise({
+  	redirectTo: '/'
+  });
 });
