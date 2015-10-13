ed6107f6bcf6dd0bbf917947d67d35a0a58e85c0
zachloubier <zach@growwithtrellis.com>
2015-10-12 20:45:25 -0400

    Create setPassword validPassword and generateJWT methods on User model

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
 	fname: String,
@@ -7,5 +9,29 @@ var UserSchema = new mongoose.Schema({
 	password: String,
 	salt: String
 });
+
+UserSchema.methods.setPassword = function(password) {
+	this.salt = crypto.randomBytes(16).toString('hex');
+
+	this.password = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+	var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+	return this.password === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+	var today = new Date();
+	var exp = new Date(today);
+	exp.setDate(today.getDate() + 60);
+
+	return jwt.sign({
+		_id: this._id,
+		username: this.username,
+		exp: parseInt(exp.getTime() / 1000)
+	}, 'SECRET');
+};
 
 mongoose.model('User', UserSchema);
