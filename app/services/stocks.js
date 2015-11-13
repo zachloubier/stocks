b389bdae73687ab6b5437ec19b7bb24832f86f97
zachloubier <zach@growwithtrellis.com>
2015-11-13 11:00:04 -0500

    Use spaces instead of tabs

--- app/services/stocks.js
+++ app/services/stocks.js
@@ -1,36 +1,37 @@
-app.factory('stocks', ['$http', function($http) {
-	return {
-		stocks: [],
-		getAll: function() {
-			var self = this;
-			return $http.get('http://localhost:3000/stocks')
-				.success(function(data) {
-					angular.copy(data, self.stocks);
-					// return data;
-				})
-				.error(function(data) {
-					return data;
-				});
-		},
+app.factory('stocks', ['$http', function ($http) {
+  return {
+    stocks: [],
 
-		get: function(symbol) {
-			var self = this;
-			return $http.get('http://localhost:3000/stocks/' + symbol)
-				.success(function(data) {
-					return data;
-				});
-		},
+    getAll: function () {
+      var self = this;
+      return $http.get('http://localhost:3000/stocks')
+        .success(function (data) {
+          angular.copy(data, self.stocks);
+          // return data;
+        })
+        .error(function (data) {
+          return data;
+        });
+    },
 
-		create: function(data) {
-			var self = this;
-			return $http.post('http://localhost:3000/stocks', data)
-				.success(function(data) {
-					self.stocks.push(data);
-				})
-				.error(function(data) {
-					return data;
-				})
-		}
-	}
+    get: function (symbol) {
+      var self = this;
+      return $http.get('http://localhost:3000/stocks/' + symbol)
+        .success(function (data) {
+          return data;
+        });
+    },
+
+    create: function (data) {
+      var self = this;
+      return $http.post('http://localhost:3000/stocks', data)
+        .success(function (data) {
+          self.stocks.push(data);
+        })
+        .error(function (data) {
+          return data;
+        })
+    }
+  }
 
 }]);
